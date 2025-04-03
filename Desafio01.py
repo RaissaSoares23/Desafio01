9d0a4b25e6d847c9c3e0ed31fe8a295349cf85df
raissa soares <raissasoaresandradedasilva@gmail.com>
2025-04-03 16:44:06 -0300

    novo exercico

---
+++ Desafio01.py
@@ -0,0 +1,25 @@
+hora1 = int (input("Me informa o primeiro horário:"))
+min1 = int(input("Me informe os minutos:"))
+hora2 = int (input ("Me informe o segundo horário:"))
+min2 = int(input (" Me informe os minutos:"))
+
+soma1= hora1 + hora2
+soma2= min1 + min2
+
+if soma2 >=60:
+    soma2=soma2+1
+    soma2 = soma2-60
+if soma1 >12:
+    soma1= soma1-12
+if soma1>24:
+    soma1=soma1-24
+
+    print ("A saida é igual a:",soma1, soma2)
+
+
+
+
+
+
+
+
