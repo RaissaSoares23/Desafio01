402eea1e7cff0989f3776782dbcae5d900fb7648
raissa soares <raissasoaresandradedasilva@gmail.com>
2025-04-08 14:12:17 -0300

    atualizar o desafio

--- Desafio01.py
+++ Desafio01.py
@@ -7,14 +7,14 @@ soma1= hora1 + hora2
 soma2= min1 + min2
 
 if soma2 >=60:
-    soma2=soma2+1
+    soma1=soma1+1
     soma2 = soma2-60
 if soma1 >12:
     soma1= soma1-12
 if soma1>24:
     soma1=soma1-24
 
-    print ("A saida é igual a:",soma1, soma2)
+print ("A saida é igual a:",soma1, soma2)
 
 
 
